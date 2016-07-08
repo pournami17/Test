e4aa39414ef98f07102cda74c8ba563eaea623a1
Pournami <pournami@qburst.com>
2016-07-08 16:46:30 +0530

    Changes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 window.onload = function(){
     enableBtn();
     geoFindLocation();
-    initialize();
 };
 
 loadJson('project.json',"projectList");
@@ -14,7 +13,6 @@ var hoursBurned = 0;
 var minsBurned = 0;
 var total = 8;
 var showEntry = [];
-var geocoder;
 
 //Function to load multiple JSON
 
@@ -63,7 +61,6 @@ function success(pos){
       var longitude = pos.coords.longitude;
       console.log("latitude ", latitude);
       console.log("longitude ", longitude);
-      codeLatLng(latitude, longitude);
       var today;
       var dateArray = [];
     
@@ -99,58 +96,6 @@ function success(pos){
 function geoFindLocation() {
   navigator.geolocation.getCurrentPosition(success, error);
 }
-
-function initialize() {
-    geocoder = new google.maps.Geocoder();
-}
-
-  function codeLatLng(lat, lng) {
-
-    var latlng = new google.maps.LatLng(lat, lng);
-    geocoder.geocode({'latLng': latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-      //console.log(results);
-        if (results[1]) {
-        var indice=0;
-        for (var j=0; j<results.length; j++)
-        {
-            if (results[j].types[0]=='locality')
-                {
-                    indice=j;
-                    break;
-                }
-            }
-        alert('The good number is: '+j);
-        console.log(results[j]);
-        for (var i=0; i<results[j].address_components.length; i++)
-            {
-                if (results[j].address_components[i].types[0] == "locality") {
-                        //this is the object you are looking for
-                        city = results[j].address_components[i];
-                    }
-                if (results[j].address_components[i].types[0] == "administrative_area_level_1") {
-                        //this is the object you are looking for
-                        region = results[j].address_components[i];
-                    }
-                if (results[j].address_components[i].types[0] == "country") {
-                        //this is the object you are looking for
-                        country = results[j].address_components[i];
-                    }
-            }
-
-            //city data
-            alert(city.long_name + " || " + region.long_name + " || " + country.short_name)
-
-
-            } else {
-              alert("No results found");
-            }
-        //}
-      } else {
-        alert("Geocoder failed due to: " + status);
-      }
-    });
-  }
 
 document.getElementById("submitBtn").addEventListener("click", function(event){
     event.preventDefault();
