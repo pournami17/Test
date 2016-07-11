cbd75ac9d417adec0d1a08d9cc767f10d6df0303
Pournami <pournami@qburst.com>
2016-07-11 11:10:07 +0530

    Fixed issues

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 
 window.onload = function(){
-    enableBtn();
-    geoFindLocation();
-    initialize();
+    defaultDate();
 };
 
 loadJson('project.json',"projectList");
 loadJson('activityType.json',"activityList");
-
 
 var prevDate;
 var hoursBurned = 0;
@@ -55,105 +52,41 @@ function loadSelect(populateList,divID){
   
 }
 
-function initialize(){
-    geocoder = new google.maps.Geocoder();
-}
+// Function to display last seven dates in Date Select box
 
-// Function to display last seven dates in Date Select box if geolocation is successful
+function defaultDate(){
 
-function success(pos){
-      var lat = pos.coords.latitude;
-      var lng = pos.coords.longitude;
-      console.log("latitude ", latitude);
-      console.log("longitude ", longitude);
-      var today;
-      var dateArray = [];
+    var today;
+    var dateArray = [];
+    var dateOptions = {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+    };
 
-      var latlng = new google.maps.LatLng(lat, lng);
-      geocoder.geocode({'latLng': latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-      console.log(results)
-        if (results[1]) {
-         //formatted address
-         alert(results[0].formatted_address)
-        //find country name
-             for (var i=0; i<results[0].address_components.length; i++) {
-            for (var b=0;b<results[0].address_components[i].types.length;b++) {
-
-            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-                    //this is the object you are looking for
-                    city= results[0].address_components[i];
-                    break;
-                }
-            }
-        }
-        //city data
-        alert(city.short_name + " " + city.long_name)
-        } else {
-          alert("No results found");
-        }
-      } else {
-        alert("Geocoder failed due to: " + status);
-      }
-      });
-    
-      for (i=0 ; i<8 ; i++) {
+    for (i=0 ; i<8 ; i++) {
         today = new Date();
         day = today.getDate();
         var olderDate = new Date(today.setDate(day - i)); //Setting Dates
-        if(!(lat == 8.5499495)&&(lng == 76.87785)) {
-            dateArray.push( ('0' + olderDate.getDate()).slice(-2) + '/' + ('0' + (olderDate.getMonth()+1)).slice(-2) + '/' + olderDate.getFullYear());
-        }
-        else {
-          dateArray.push(('0' + (olderDate.getMonth()+1)).slice(-2) + '/' + ('0' + olderDate.getDate()).slice(-2) + '/' +  olderDate.getFullYear());
-        }
-        date = dateArray[i];
-      }
-
-      for (j=7 ; j >=0; j--) {
-          var opt = document.createElement("option");
-          opt.text = dateArray[j];
-          opt.value = j;
-          var select =document.getElementById("dateList");
-          select.appendChild(opt);
-      }
-      select.selectedIndex = 7;
-  }
-
-  function error(err) {
-      console.log('ERROR (' + err.code + '):' + err.message);
-  }
-
-//Function to find user location
-
-function geoFindLocation() {
-  navigator.geolocation.getCurrentPosition(success, error);
+        
+            dateArray.push(olderDate.toLocaleDateString("en-US", dateOptions));
+        
+      
+    }
+    for (j=7 ; j >=0; j--) {
+        var opt = document.createElement("option");
+        opt.text = dateArray[j];
+        opt.value = j;
+        var select =document.getElementById("dateList");
+        select.appendChild(opt);
+    }
+    select.selectedIndex = 7;
 }
 
 document.getElementById("submitBtn").addEventListener("click", function(event){
     event.preventDefault();
     submitStatusForm();
 });
-
-document.getElementById("message").addEventListener("keyup", function(){
-    var description = document.getElementById('message');
-    var counterID = document.getElementById('counter');
-    var maxLimit = 20;
-    textCounter(description, counterID, maxLimit);
-    enableBtn();
-});
-
-//Function to enable/disable SAVE btn
-
-function enableBtn() {
-    if(document.getElementById("message").value != '') {
-      document.getElementById("submitBtn").disabled = false;
-    }
-    else {
-      document.getElementById("submitBtn").disabled = true;
-    }
-}
 
 //Function for validation
 
@@ -164,19 +97,6 @@ function validateFields() {
     return false;
   }
   return true;
-}
-
-// Function to calculate remaining characters 
-
-function textCounter(elDescription, elCounter, elMaxLimit) {
-  var elDescriptionValLength = elDescription.value.replace(/\s/g, '').length;
-  var spaces = elDescription.value.split(' ').length - 1;
-    if(elDescriptionValLength > elMaxLimit ){
-      elDescription.value = elDescription.value.substr(0, elMaxLimit + spaces);
-    }
-    else {
-        elCounter.value = elMaxLimit - elDescriptionValLength;
-    }
 }
 
 //Function to submit form
@@ -191,8 +111,7 @@ function submitStatusForm() {
             hrs = document.getElementById('hours'),
             mins = document.getElementById('minutes'),
             msg = document.getElementById('message');
-            
-        // var maxLimit = 10;
+
 
         var date = dateList.options[dateList.selectedIndex].text,
             dateVal = dateList.options[dateList.selectedIndex].value,
@@ -201,10 +120,8 @@ function submitStatusForm() {
             timeHrs = hrs.options[hrs.selectedIndex].text,
             timeHrsVal = hrs.options[hrs.selectedIndex].value,
             timeMinutes = mins.options[mins.selectedIndex].text,
-            timeMinutesVal = mins.options[mins.selectedIndex].value,
+            timeMinutesVal = mins.options[mins.selectedIndex].value
             description = msg.value;
-
-        // textCounter(description, counter, maxLimit);
         
         showEntry.push({date, project, activity, timeHrs, timeMinutes, description});
 
@@ -224,8 +141,6 @@ function submitStatusForm() {
         }
         document.getElementById('displayLog').innerHTML = setContent;
         clearText();
-        document.getElementById('counter').value = 20;
-        enableBtn();
     }
     
 }
