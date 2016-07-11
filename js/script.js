cb2882a2c35867a692b712b39458e5ae1b39f9d9
Pournami <pournami@qburst.com>
2016-07-11 11:00:07 +0530

    added reverse geocoding

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 window.onload = function(){
     enableBtn();
     geoFindLocation();
+    initialize();
 };
 
 loadJson('project.json',"projectList");
@@ -54,21 +55,54 @@ function loadSelect(populateList,divID){
   
 }
 
+function initialize(){
+    geocoder = new google.maps.Geocoder();
+}
+
 // Function to display last seven dates in Date Select box if geolocation is successful
 
 function success(pos){
-      var latitude = pos.coords.latitude;
-      var longitude = pos.coords.longitude;
+      var lat = pos.coords.latitude;
+      var lng = pos.coords.longitude;
       console.log("latitude ", latitude);
       console.log("longitude ", longitude);
       var today;
       var dateArray = [];
+
+      var latlng = new google.maps.LatLng(lat, lng);
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+      console.log(results)
+        if (results[1]) {
+         //formatted address
+         alert(results[0].formatted_address)
+        //find country name
+             for (var i=0; i<results[0].address_components.length; i++) {
+            for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                    //this is the object you are looking for
+                    city= results[0].address_components[i];
+                    break;
+                }
+            }
+        }
+        //city data
+        alert(city.short_name + " " + city.long_name)
+        } else {
+          alert("No results found");
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+      });
     
       for (i=0 ; i<8 ; i++) {
         today = new Date();
         day = today.getDate();
         var olderDate = new Date(today.setDate(day - i)); //Setting Dates
-        if(!(latitude == 8.5499495)&&(longitude == 76.87785)) {
+        if(!(lat == 8.5499495)&&(lng == 76.87785)) {
             dateArray.push( ('0' + olderDate.getDate()).slice(-2) + '/' + ('0' + (olderDate.getMonth()+1)).slice(-2) + '/' + olderDate.getFullYear());
         }
         else {
@@ -88,7 +122,7 @@ function success(pos){
   }
 
   function error(err) {
-      console.warn('ERROR (' + err.code + '):' + err.message);
+      console.log('ERROR (' + err.code + '):' + err.message);
   }
 
 //Function to find user location
